62cec8c2c42644f2116c7189b6c7543ad4b88014
edugichuki <muriithigichuki854@gmail.com>
2023-09-26 17:00:52 +0300

    Edit files

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 import SearchIcon from "../search.svg";
 import MovieCard from "./MovieCard";
 
-require("dotenv").config({ path: "../env" });
+const API_URL = "http://www.omdbapi.com?apikey=d0402411";
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.API_KEY}`;
-
-const movie1 = {
-  Title: "Spiderman the Verse",
-  Year: "2019–",
-  imdbID: "tt12122034",
-  Type: "series",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg",
-};
+// const movie1 = {
+//   Title: "Spiderman the Verse",
+//   Year: "2019–",
+//   imdbID: "tt12122034",
+//   Type: "series",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg",
+// };
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -32,7 +30,7 @@ const App = () => {
     setMovies(data.Search);
   };
   useEffect(() => {
-    searchMovies("Spiderman");
+    searchMovies("Titanic");
   }, []);
   return (
     <div className="app">
